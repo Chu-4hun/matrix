b2cd6c856dda9b71db465094e1e366eaf9bce184
Данила <Tapok134224@mail..ru>
2020-10-18 22:02:26 +0300

    task5

--- Matrix1/Program.cs
+++ Matrix1/Program.cs
@@ -4,11 +4,13 @@ using static Matrix1.task1;
 using static Matrix1.task2;
 using static Matrix1.task3;
 using static Matrix1.task4;
+using static Matrix1.TreeNode;
+
 
 
 namespace Matrix1
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -22,6 +24,21 @@ namespace Matrix1
             Thread.Sleep(5000);
             Console.Clear();
             four();
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.Write("n = ");
+                var n = int.Parse(Console.ReadLine());
+                var a = new int[n];
+                var random = new Random();
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = random.Next(0, 100);
+                }
+
+                Console.WriteLine("Random Array: {0}", string.Join(" ", a));
+
+                Console.WriteLine("Sorted Array: {0}", string.Join(" ", TreeSort(a)));
+            
             Thread.Sleep(5000);
             Console.Clear();
             task6();
